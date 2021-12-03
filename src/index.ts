dbfdfb1a3baef16ffb9ebe0e8f7a671a10ec8c7f
JuanCRdrums <juankdrums14@gmail.com>
2021-12-03 12:41:11 -0500

    Change to solana open RPC and 30 seconds interval

--- src/index.ts
+++ src/index.ts
@@ -71,7 +71,7 @@ async function collectEventQueue(m: MarketConfig, r: RedisConfig) {
       console.error(m.marketName, err.toString())
     }
     await sleep({
-      Seconds: process.env.INTERVAL ? parseInt(process.env.INTERVAL) : 20,
+      Seconds: process.env.INTERVAL ? parseInt(process.env.INTERVAL) : 30,
     })
   }
 }
@@ -86,7 +86,7 @@ if (redisUrl.password !== '') {
 
 const network = 'mainnet-beta'
 const clusterUrl =
-  process.env.RPC_ENDPOINT_URL || 'https://connect.runnode.com/?apikey=7Tgub7ENXRXSBpzN6FXJ'
+  process.env.RPC_ENDPOINT_URL || 'https://solana-api.projectserum.com'
 const programIdV3 = '9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin'
 
 
